7a895c188adc171977c29947a1a3ce05baec0e64
lorevlmt <lore.vimontes@gmail.com>
2024-11-11 17:36:54 +0100

    Ejercicio_11 Resuelto

--- src/main/java/com/campusdual/classroom/TVRemote.java
+++ src/main/java/com/campusdual/classroom/TVRemote.java
@@ -2,4 +2,61 @@ package com.campusdual.classroom;
 
 public class TVRemote {
 
+    int channel;
+    int volume;
+    boolean on;
+    String color;
+
+    public TVRemote(String color) {
+        this.color = color;
+        this.channel = 0;
+        this.volume = 0;
+    }
+
+    public boolean turnOn() {
+        this.on = true;
+        return this.on;
+    }
+
+    public boolean turnOff() {
+        this.on = false;
+        return this.on;
+    }
+
+    public void channelUp() {
+        this.channel++;
+    }
+
+    public void channelDown() {
+        if (this.checkMinValue(this.channel)) {
+            this.channel--;
+        } else {
+            System.out.println("El canal ya está al mínimo");
+        }
+    }
+
+    public void volumeUp() {
+        this.volume++;
+    }
+
+    public void volumeDown() {
+        if (this.checkMinValue(this.volume)) {
+            this.volume--;
+        } else {
+            System.out.println("El volumen ya está al mínimo");
+        }
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    private boolean checkMinValue(int value) {
+        if (value == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
